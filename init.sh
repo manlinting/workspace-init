a33c1df51a3cab6dea14e39bd4d7ea2353fecc08
root <root@152651.sys.ipv4.io>
2016-11-08 12:24:59 -0500

    fix

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/bin/sh 
 yum install -y telnet lszrz git cmake wget ncurses-devel python-devel
 
+
+yum install python-setuptools  && easy_install pip
+pip install shadowsocks
+
+ssserver -p 443 -k 504504 -m aes-256-cfb --user nobody -d start
